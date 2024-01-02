8cd68b5cc15914773ee035941aa63d5b59f9f0f4
dannydeadd <98296978+dannydeadd@users.noreply.github.com>
2024-01-01 18:57:31 -0600

    shop section work progress

--- src/app/components/main.tsx
+++ src/app/components/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Image from 'next/image'
 
+
 const Main = () => {
   return (
     <section>
-        <div className="px-12 py-12 text-center md:px-12 lg:text-left">
+        <div className="px-12 py-32 text-center md:px-12 lg:text-left">
           <div className="w-100 mx-auto sm:max-w-2xl md:max-w-3xl lg:max-w-5xl xl:max-w-7xl">
             <div className="grid items-center gap-12 lg:grid-cols-2">
               <div className="mt-12 lg:mt-0">
@@ -21,9 +22,24 @@ const Main = () => {
                 active:text-primary-700 dark:hover:bg-primary-800 dark:hover:bg-opacity-60"
                   data-te-ripple-init data-te-ripple-color="light" href="#!" role="button">Learn more</a>
               </div>
-              <div className="mt-10 lg:mb-0">
+              <div className=" mt-0 lg:mb-0">
               <svg className="absolute stroke-[1.3px] mt-[21px]" xmlns="http://www.w3.org/2000/svg" width="500" height="500" viewBox="0 0 500 500" fill="none">
                   <path d="M143.385 479.93L143.385 479.929C86.1603 457.468 37.9742 403.063 14.9662 333.777C-7.95257 264.576 -5.57458 180.566 36.1367 115.425C77.9457 50.1815 159.226 3.66055 244.33 0.656615C329.356 -2.3478 418.26 38.0833 460.778 103.39C503.292 168.78 499.498 259.152 470.485 326.715C441.489 394.148 387.218 438.755 326.908 465.895C266.679 492.947 200.512 502.389 143.385 479.93Z" stroke="#673f78"/>
+              </svg>
+              <svg className="absolute mt-[360px] right-[680px]" width="104" height="30" viewBox="0 0 104 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="12.2021" width="5" height="30" transform="rotate(24 12.2021 0)" fill="#673f78"/>
+                <rect x="33.9719" width="5" height="30" transform="rotate(24 33.9719 0)" fill="#673f78"/>
+                <rect x="55.7418" width="5" height="30" transform="rotate(24 55.7418 0)" fill="#673f78"/>
+                <rect x="77.5116" width="5" height="30" transform="rotate(24 77.5116 0)" fill="#673f78"/>
+                <rect x="99.2813" width="5" height="30" transform="rotate(24 99.2813 0)" fill="#673f78"/>
+              </svg>
+              <svg className="absolute ml-[450px]" width="50" height="56" viewBox="0 0 50 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <rect x="9" y="16" width="6" height="24" rx="3" fill="#673f78"/>
+                  <rect y="31" width="6" height="24" rx="3" transform="rotate(-90 0 31)" fill="#673f78"/>
+                  <rect x="35" y="32" width="6" height="24" rx="3" fill="#673f78"/>
+                  <rect x="26" y="47" width="6" height="24" rx="3" transform="rotate(-90 26 47)" fill="#673f78"/>
+                  <rect x="35" width="6" height="24" rx="3" fill="#673f78"/>
+                  <rect x="26" y="15" width="6" height="24" rx="3" transform="rotate(-90 26 15)" fill="#673f78"/>
               </svg>
 
                 {/* <img src="https://jewelrydesigns.com/wp-content/uploads/CJLP-3-Explore-Jewelry-Designs.jpg"
@@ -36,6 +52,7 @@ const Main = () => {
                   alt="Picture of jewels"
                   className="ml-[13px]"
                   />
+
               </div>
             </div>
           </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Main from './components/main'
+import Shop from './components/shop'
 
 export default function Home() {
   return (
     <main>
       <Main />
+      <Shop />
     </main>
   )
 }
