461b43ff562fdf549ac358378022a4181c8c72e5
dannydeadd <98296978+dannydeadd@users.noreply.github.com>
2024-01-03 03:54:51 -0600

    about section added, shop section more work done

---
+++ src/app/components/about.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="bg-primary w-full h-full px-32 py-40">
+        <h1 className="text-6xl">About us</h1>
+        <p className="py-10">In the realm of shadows and the whispers of moonlit nights, we unveil Crowsfeet Jewerly, a sanctuary for those who embrace the beauty within darkness. We are more than just a jewelry brand; we are the keepers of mystery, the weavers of stories, and the architects of adornments that speak to the depths of your soul.
+
+Here at Crowsfeet Jewerly, our mission is to transcend the ordinary and immerse you in a world where each piece of jewelry is a manifestation of your unique spirit. We draw inspiration from the enigmatic allure of gothic aesthetics, blending it seamlessly with elements of timeless elegance. Our creations are not just accessories; they are artifacts that carry the essence of the extraordinary.
+
+Our journey began in the depths of creative passion, fueled by a desire to break free from the conventional. Every piece in our collection is meticulously crafted, infused with the alchemy of craftsmanship and a touch of the mysterious. Whether you are drawn to the iridescence of dark crystals, the sharp elegance of silver, or the romanticism of intricate designs, Crowsfeet Jewerly has something exceptional to offer.
+
+As advocates of self-expression, we believe that jewelry is a personal narrative, a silent proclamation of your identity. Each piece you choose from Crowsfeet Jewerly is a statement—an emblem of your strength, your rebellion, and your unique sense of style.
+
+Join us on this journey through the shadows, where we celebrate the beauty found in the unconventional. Let Crowsfeet Jewerly be the companion on your quest for individuality, as we continue to redefine the boundaries of jewelry artistry.
+
+Thank you for being a part of our dark, dazzling tapestry.</p>
+    </section>
+  )
+}
+
+export default About
