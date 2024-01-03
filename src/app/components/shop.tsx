40c69be13d7a6ad117e7334779196bf04bc428ae
dannydeadd <98296978+dannydeadd@users.noreply.github.com>
2024-01-03 03:51:33 -0600

    about section and shop section

--- src/app/components/shop.tsx
+++ src/app/components/shop.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Image from 'next/image'
 import { useState } from 'react';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Shop = () => {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Main from './components/main'
 import Shop from './components/shop'
+import About from './components/about'
 
 export default function Home() {
   return (
     <main>
       <Main />
       <Shop />
+      <About />
     </main>
   )
 }
